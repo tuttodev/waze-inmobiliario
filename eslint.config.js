9916c0cc43379a89e293194df1c9790e1ad07548
tuttodev <jsebas2426@gmail.com>
2025-04-07 09:06:15 -0500

    update eslint configuration to disable no-unsafe-assignment rule

--- eslint.config.js
+++ eslint.config.js
@@ -25,7 +25,8 @@ export default defineConfig([
       '@typescript-eslint/no-unsafe-argument': 'off',
       '@typescript-eslint/no-unsafe-member-access': 'off',
       '@typescript-eslint/no-unsafe-return': 'off',
-      '@typescript-eslint/no-magic-numbers': 'off'
+      '@typescript-eslint/no-magic-numbers': 'off',
+      '@typescript-eslint/no-unsafe-assignment': 'off'
     }
   }
 ])
